c720695c11060b1460a87e2c23175ddd76f8befd
Prateek Bhustali <61668356+omniscientoctopus@users.noreply.github.com>
2024-08-20 11:02:35 +0200

    Minor fix: Update rollout convention in rollout.py example

--- examples/rollout.py
+++ examples/rollout.py
@@ -10,7 +10,6 @@ import jax_imprl.structural_envs as structural_envs
 env = structural_envs.make(setting="hard-5-of-5")
 
 key = jax.random.PRNGKey(42)
-key, subkey = jax.random.split(key)
 
 action = jnp.array([0, 0, 0, 0, 0])
 
@@ -18,20 +17,20 @@ time0 = time.time()
 store = []
 for ep in range(100):
 
-    # Reset the environment
-    obs, state = env.reset(subkey)
+    # reset
     key, subkey = jax.random.split(key)
+    obs, state = env.reset(subkey)
+
     done = False
     total_reward = 0
 
     while not done:
 
-        # step the environment
-        obs, state, reward, done, info = env.step(subkey, state, action)
+        # generate keys for next timestep
+        key, step_keys = env.split_key(key)
+        obs, state, reward, done, _ = env.step_env(step_keys, state, action)
 
         total_reward += reward
-
-        key, subkey = jax.random.split(key)
 
     print(f"Episode: {ep}, Total Reward: {total_reward}")
     store.append(total_reward)
